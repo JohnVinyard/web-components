03077abcc4ee61e379143e8716d7c8c50edd338d
John Vinyard <johnwho@gmail.com>
2024-08-16 13:22:57 -0500

    Add a simple Python code block component

--- build/components/index.js
+++ build/components/index.js
@@ -2,5 +2,5 @@ import { AudioView } from './audioview';
 import { ScatterPlot } from './scatterplot';
 import { TensorView } from './tensor';
 import { CodeBlock } from './codeblock';
-
 export { AudioView, ScatterPlot, TensorView, CodeBlock };
+//# sourceMappingURL=index.js.map
---
+++ components/codeblock.ts
@@ -0,0 +1,65 @@
+import hljs from 'highlight.js';
+import python from 'highlight.js/lib/languages/python';
+hljs.registerLanguage('python', python);
+
+export class CodeBlock extends HTMLElement {
+    public language: string | null;
+
+    constructor() {
+        super();
+        this.language = null;
+    }
+
+    public render() {
+        let shadow: ShadowRoot | null = this.shadowRoot;
+
+        if (!shadow) {
+            shadow = this.attachShadow({ mode: 'open' });
+        }
+
+        setTimeout(() => {
+            const highlightedCode = hljs.highlight(this.textContent, {
+                language: this.language,
+            });
+
+            // choose a theme from here: https://www.jsdelivr.com/package/npm/highlightjs-themes?tab=files
+            shadow.innerHTML = `
+                <div>
+                    <link
+                        rel="stylesheet"
+                        href="https://cdn.jsdelivr.net/npm/highlightjs-themes@1.0.0/darkula.css"
+                    />
+                    <pre class="hljs"><code>${highlightedCode.value}</code></pre>
+                </div>
+            `;
+            // KLUDGE: What's a more principled way to know when this.textContent is available?
+        }, 1000);
+    }
+
+    public connectedCallback() {
+        this.render();
+    }
+
+    public static get observedAttributes(): (keyof CodeBlock)[] {
+        return ['language'];
+    }
+
+    public attributeChangedCallback(
+        property: string,
+        oldValue: string,
+        newValue: string
+    ) {
+        if (newValue === oldValue) {
+            return;
+        }
+
+        this[property] = newValue;
+
+        if (property === 'language') {
+            this.render();
+            return;
+        }
+    }
+}
+
+window.customElements.define('code-block', CodeBlock);
